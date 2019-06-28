8b488d8aee2de6dfa229dc3c1af652c5bb8f931b
junblee <topguide82@gmail.com>
2019-06-28 23:16:59 +0900

    konlpy

--- 10 konlpy/main.py
+++ 10 konlpy/main.py
@@ -1,11 +1,18 @@
 from konlpy.tag import Okt
 
-ok = Okt() # ok객체에 Okt패키지 할당
-test = "안녕하세요. 오늘은 6월 입니다" # 문자열 객체
+ok = Okt() 
 
-# ok.pos(test)은 리스트로 반환 
-# pos메서드때문에 리스트 각각이 튜플로 반환
-# 리스트이므로 반복문으로 하나씩 꺼낼수있음
-for i in ok.pos(test) : 
+""" 
+# 문자열에서 해시태그만을 추출
+test = "안녕하세요. 오늘은 6월 입니다 #hash #man #Yoman"
+
+for i in ok.pos(test,norm=True ) :
+    if i[1] == 'Hashtag' : # 튜플의 첫번쨰 요소가 해시태그라면
+        print(i)
+"""
+# 명사만을 추출해보자
+# 리스트로 반환된다
+
+test = "와썹 프로그래밍 요맨"
+for i in ok.nouns(test) : # 고유명사를 기준으로 리스토로 반환해주는 nouns인자
     print(i)
-
