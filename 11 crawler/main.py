f901a52ec99e7e2561b03506bb94c4437448891c
junblee <topguide82@gmail.com>
2019-06-30 20:38:14 +0900

    rank

--- 11 crawler/main.py
+++ 11 crawler/main.py
@@ -1,5 +1,6 @@
 import requests
 from bs4 import BeautifulSoup as bs
+from konlpy.tag import Okt # 말뭉치검색위해
 
 URL = "https://academy.nomadcoders.co/p/instaclone-2-0"
 
@@ -22,6 +23,29 @@ def get_tags() :
     else : 
         raise Exception('응답 코드가 다릅니다')
                 
-        
+# 함수호출 > tags_list반환 > 데이터 처리 함수 작성
+# 불러온 목록에서 명사를 추출
+# 추출한 명사 중 가장 많이 나온 명사 랭킹화 출력
+def get_rank() : 
+    tags_list = get_tags() # 함수를 통해 문자값을 넘겨받는 객체
+    ok = Okt()
+    rank = {} # 랭크매길 딕셔너리, 명사값처리
+    for tag in tags_list : 
+        noun = ok.nouns(tag) # 명사를 따로 구분지음. 고로 리스트
+        for n in noun : # noun도 리스트이기에 반복문으로 처리가능
+            if not (n in rank) : # 랭크dic에 명사값이 없다면
+                rank[n] = 0 # n할당하여 초기화
+            rank[n] += 1 # 있다면 랭크값에 1씩 더해줌
+    
+    # 랭크구하기
+    # 내림차순
+    rank = sorted(rank.items(), key = lambda x:x[1], reverse = True)
+    
+    # 정렬 결과, 키 값 형태로 포맷
+    for k,v in rank[1:15]: # 1위 ~ 15위까지 한 줄
+        print("{}({})".format(k,v), 둥 = ' ')
 
- 
+# get_rank()함수 호출
+if __name__ == "__main__" : 
+    get_rank()
+
